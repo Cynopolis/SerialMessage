7a095fb42090261b17dbe592465706aa54e454e2
Quinn <quinn.henthorne@gmail.com>
2025-07-06 15:32:56 -0400

    Ran through a formatter

--- BluetoothSerialMessage.h
+++ BluetoothSerialMessage.h
@@ -3,58 +3,65 @@
 #include <BluetoothSerial.h>
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-class BluetoothSerialMessage : public SerialMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>{
-    public:
-        /**
-         * @brief Construct a new Bluetooth Serial Message object
-         */
-        BluetoothSerialMessage(BluetoothSerial *serial);
-        
-        void Init(uint32_t baudRate) override{this->Init("BluetoothMessage");}
-        /**
-         * @brief Initialize the BluetoothSerialMessage object
-         */
-        void Init(const char * bluetoothName);
+class BluetoothSerialMessage : public SerialMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>
+{
+public:
+    /**
+     * @brief Construct a new Bluetooth Serial Message object
+     */
+    BluetoothSerialMessage(BluetoothSerial *serial);
 
-        /**
-         * @brief prints the args array to the serial monitor
-        */
-        void PrintArgs() override;
-    
-    private:
-        /**
-         * @brief reads the serial data and stores it in the data array
-        */
-        char getChar() override;
-        uint32_t dataAvailable() override;
+    void Init(uint32_t baudRate) override { this->Init("BluetoothMessage"); }
+    /**
+     * @brief Initialize the BluetoothSerialMessage object
+     */
+    void Init(const char *bluetoothName);
 
-        BluetoothSerial *serial;
+    /**
+     * @brief prints the args array to the serial monitor
+     */
+    void PrintArgs() override;
+
+private:
+    /**
+     * @brief reads the serial data and stores it in the data array
+     */
+    char getChar() override;
+    uint32_t dataAvailable() override;
+
+    BluetoothSerial *serial;
 };
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-void BluetoothSerialMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::Init(const char * bluetoothName){
+void BluetoothSerialMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::Init(const char *bluetoothName)
+{
     serial->begin(bluetoothName);
 }
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-char BluetoothSerialMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::getChar(){
+char BluetoothSerialMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::getChar()
+{
     return serial->read();
 }
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-uint32_t BluetoothSerialMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::dataAvailable(){
+uint32_t BluetoothSerialMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::dataAvailable()
+{
     return serial->available();
 }
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-BluetoothSerialMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::BluetoothSerialMessage(BluetoothSerial *serial){
+BluetoothSerialMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::BluetoothSerialMessage(BluetoothSerial *serial)
+{
     this->serial = serial;
 }
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-void BluetoothSerialMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::PrintArgs(){
+void BluetoothSerialMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::PrintArgs()
+{
     serial->print("Current number of args: ");
     serial->println(this->populatedArgs);
-    for (int i = 0; i < this->populatedArgs; i++) {
+    for (int i = 0; i < this->populatedArgs; i++)
+    {
         serial->print(this->args[i]);
         serial->print(" ");
     }
--- TelnetMessage.h
+++ TelnetMessage.h
@@ -7,89 +7,97 @@
 #include "GlobalPrint.h"
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-class TelnetMessage : public Message<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>{
-    public:
-        /**
-         * @brief Construct a new Bluetooth Serial Message object
-         */
-        TelnetMessage(ESPTelnet *telnet) : 
-        telnet(telnet){}
+class TelnetMessage : public Message<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>
+{
+public:
+    /**
+     * @brief Construct a new Bluetooth Serial Message object
+     */
+    TelnetMessage(ESPTelnet *telnet) : telnet(telnet) {}
 
-        void Init(uint32_t baudRate) override{Serial.println("Never call this without a parameter, this will not function properly without a callback function");}
-        /**
-         * @brief Initialize the TelnetMessage object
-         * @pre The WiFi object must have a state of WL_CONNECTED
-         */
-        void Init(void (*callback)(String data));
+    void Init(uint32_t baudRate) override { Serial.println("Never call this without a parameter, this will not function properly without a callback function"); }
+    /**
+     * @brief Initialize the TelnetMessage object
+     * @pre The WiFi object must have a state of WL_CONNECTED
+     */
+    void Init(void (*callback)(String data));
 
-        /**
-         * @brief prints the args array to the telnet monitor
-        */
-        void PrintArgs() override;
+    /**
+     * @brief prints the args array to the telnet monitor
+     */
+    void PrintArgs() override;
 
-        void SetString(const char * data);    
+    void SetString(const char *data);
 
-        void SetOnInputRecieved(void (*callback)(String data));
-    private:
-        /**
-         * @brief reads the telnet data and stores it in the data array
-        */
-        char getChar() override;
+    void SetOnInputRecieved(void (*callback)(String data));
 
-        /**
-         * @brief returns the amount of data available
-         */
-        uint32_t dataAvailable() override;
+private:
+    /**
+     * @brief reads the telnet data and stores it in the data array
+     */
+    char getChar() override;
 
-        static void onConnectCallback(String ip);
-        static void onConnectionAttemptCallback(String ip);
-        static void onReconnectCallback(String ip);
-        static void onDisconnectCallback(String ip);
+    /**
+     * @brief returns the amount of data available
+     */
+    uint32_t dataAvailable() override;
 
-        ESPTelnet *telnet;
+    static void onConnectCallback(String ip);
+    static void onConnectionAttemptCallback(String ip);
+    static void onReconnectCallback(String ip);
+    static void onDisconnectCallback(String ip);
 
-        const char * incomingData = nullptr;
-        uint32_t incomingDataLength{0};
-        uint32_t incomingDataCharIndex{0};
+    ESPTelnet *telnet;
+
+    const char *incomingData = nullptr;
+    uint32_t incomingDataLength{0};
+    uint32_t incomingDataCharIndex{0};
 };
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::onConnectCallback(String ip) {
-  GlobalPrint::Print("- Telnet: ");
-  GlobalPrint::Print(ip);
-  GlobalPrint::Println(" connected");
+void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::onConnectCallback(String ip)
+{
+    GlobalPrint::Print("- Telnet: ");
+    GlobalPrint::Print(ip);
+    GlobalPrint::Println(" connected");
 }
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::onConnectionAttemptCallback(String ip) {
-  GlobalPrint::Print("- Telnet: ");
-  GlobalPrint::Print(ip);
-  GlobalPrint::Println(" failed to connect");
+void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::onConnectionAttemptCallback(String ip)
+{
+    GlobalPrint::Print("- Telnet: ");
+    GlobalPrint::Print(ip);
+    GlobalPrint::Println(" failed to connect");
 }
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::onReconnectCallback(String ip) {
-  GlobalPrint::Print("- Telnet: ");
-  GlobalPrint::Print(ip);
-  GlobalPrint::Println(" reconnected");
+void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::onReconnectCallback(String ip)
+{
+    GlobalPrint::Print("- Telnet: ");
+    GlobalPrint::Print(ip);
+    GlobalPrint::Println(" reconnected");
 }
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::onDisconnectCallback(String ip) {
-  GlobalPrint::Print("- Telnet: ");
-  GlobalPrint::Print(ip);
-  GlobalPrint::Println(" disconnected");
+void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::onDisconnectCallback(String ip)
+{
+    GlobalPrint::Print("- Telnet: ");
+    GlobalPrint::Print(ip);
+    GlobalPrint::Println(" disconnected");
 }
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::SetOnInputRecieved(void (*callback)(String data)){
+void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::SetOnInputRecieved(void (*callback)(String data))
+{
     this->telnet->onInputReceived(callback);
 }
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-char TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::getChar(){
+char TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::getChar()
+{
     // Stop us from reading any more data if there's nothing more to read
-    if(this->incomingData == nullptr || this->incomingDataLength == 0){
+    if (this->incomingData == nullptr || this->incomingDataLength == 0)
+    {
         return this->endMarker;
     }
 
@@ -100,7 +108,8 @@ char TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::getChar(){
 }
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::SetString(const char * data){
+void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::SetString(const char *data)
+{
     this->incomingDataLength = strlen(data);
     this->incomingDataCharIndex = 0;
     this->incomingData = data;
@@ -108,15 +117,18 @@ void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::SetString(const
 }
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-uint32_t TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::dataAvailable(){
-    if(this->incomingData == nullptr){
+uint32_t TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::dataAvailable()
+{
+    if (this->incomingData == nullptr)
+    {
         return 0;
     }
     return this->incomingDataLength - this->incomingDataCharIndex;
 }
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::Init(void (*callback)(String data)){
+void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::Init(void (*callback)(String data))
+{
     telnet->begin(23);
     // set all of our callbacks
     telnet->onConnect(TelnetMessage::onConnectCallback);
@@ -128,13 +140,14 @@ void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::Init(void (*cal
 }
 
 template <uint32_t SERIAL_BUFFER_SIZE, uint32_t MAX_ARGS, uint32_t MAX_CALLBACKS>
-void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::PrintArgs(){
+void TelnetMessage<SERIAL_BUFFER_SIZE, MAX_ARGS, MAX_CALLBACKS>::PrintArgs()
+{
     telnet->print("Current number of args: ");
     telnet->println(this->populatedArgs);
-    for (int i = 0; i < this->populatedArgs; i++) {
+    for (int i = 0; i < this->populatedArgs; i++)
+    {
         telnet->print(this->args[i]);
         telnet->print(" ");
     }
     telnet->println();
 }
-
